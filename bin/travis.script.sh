c7ee26fa94b7d7de6957db548659a833ea39d20f
Jonathan Bardo <bardo.jonathan@gmail.com>
2016-02-23 16:05:47 -0500

    Add our own codeception

---
+++ bin/travis.script.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+function run_codeception {
+
+	if [ ! -f "/tmp/wp-cli.phar" ];  then
+		download https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar /tmp/wp-cli.phar
+	fi
+
+	php /tmp/wp-cli.phar codeception run --debug
+
+}
+
+run_wp_codeception
