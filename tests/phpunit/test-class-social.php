41d7e4c6c0ddeeb3c667e4b74826a3f48d13006f
Jonathan Bardo <bardo.jonathan@gmail.com>
2016-03-22 11:22:17 -0400

    Rework phpunit test for new script enqueue

--- tests/phpunit/test-class-social.php
+++ tests/phpunit/test-class-social.php
@@ -6,9 +6,17 @@ final class TestSocial extends TestCase {
 
 	function setUp() {
 
+		global $_wp_sidebars_widgets;
+
 		parent::setUp();
 
 		$this->plugin = new Social();
+
+		if ( ! isset( $_wp_sidebars_widgets['sidebar-1']['wpcw_social'] ) ) {
+
+			$_wp_sidebars_widgets['sidebar-1'][] = 'wpcw_social-1';
+
+		}
 
 	}
 
@@ -32,14 +40,13 @@ final class TestSocial extends TestCase {
 
 	function test_widget() {
 
-		$wp_styles = wp_styles();
-
 		$instance = [
 			'title'  => 'test',
 			'labels' => [
 				'value' => 'yes',
 			],
 		];
+
 		$args = [
 			'before_widget' => '<div class="widget wpcw-widget-social"><div class="widget-content">',
 			'after_widget' => '</div><div class="clear"></div></div>',
@@ -51,6 +58,11 @@ final class TestSocial extends TestCase {
 		$this->expectOutputRegex( '/<h3 class="widget-title">/' );
 
 		$this->plugin->widget( $args, $instance );
+
+		// Tests that script & styles are enqueued enqueued
+		do_action( 'wp_enqueue_scripts' );
+
+		$wp_styles = wp_styles();
 
 		// Make sure the JS file is enqueued
 		$this->assertContains( 'font-awesome', $wp_styles->queue );
