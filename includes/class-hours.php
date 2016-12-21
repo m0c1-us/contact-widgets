e41cd5fb9c8f650e23d96942f3268faf6447a63e
Evan Herman <evan.m.herman@gmail.com>
2016-12-21 11:58:33 -0500

    Update day_order label.

--- includes/class-hours.php
+++ includes/class-hours.php
@@ -203,7 +203,7 @@ final class Hours extends Base_Widget {
 				'description'   => __( 'Enter additional information about your business.', 'contact-widgets' ),
 			],
 			'day_order' => [
-				'label'          => __( 'Current Day on Top', 'contact-widgets' ),
+				'label'          => __( 'Keep the current day at the top of the widget.', 'contact-widgets' ),
 				'type'           => 'checkbox',
 				'label_after'    => true,
 				'show_front_end' => false,
